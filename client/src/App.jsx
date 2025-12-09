381a64bfe4f315df3a00f3638af4a2240440e498
kish202 <tania2020076@gmail.com>
2025-12-09 12:38:01 +0530

    context1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/routesapp';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/Authcontext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { CartProvider } from './contexts/ShoppingContext';
 import './styles/index.css';
-
 
 function App() {
   return (
