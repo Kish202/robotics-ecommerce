837920dc9cf2fa352a8f18c7f203c320d718629f
kish202 <tania2020076@gmail.com>
2025-12-09 12:59:55 +0530

    context file

--- client/src/pages/adminPage.jsx
+++ client/src/pages/adminPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuthContext } from '../contexts/AuthContext';
+import { useAuthContext } from '../contexts/Authcontext';
 import { LogIn, AlertCircle, Loader } from 'lucide-react';
 
 const AdminLogin = () => {
